5855b50a52122ba51e6c2ccac739109a2675ca33
aitor.aperribai@gmail.com <aitor.aperribai@gmail.com@eef8f9cb-b435-6639-adcc-9dd900e9a84f>
2011-06-29 15:00:34 +0000

    prediction panel.

---
+++ trunk/src/controller/Probak.java
@@ -0,0 +1,12 @@
+package controller;
+
+import view.PredictionPanel;
+
+public class Probak {
+	 public static void main(String args[]) {
+	    }
+	 public Probak(){
+		 PredictionPanel app = PredictionPanel.getMainPanel();
+		 app.getTextArea().getTextListeners()
+	 }
+}
